531e3a553838206cc1271f52838f34ec90c78e88
pcycho <kemalkanok@gmail.com>
2012-06-12 23:19:30 -0700

    new sql with innerjoin and ratio calc system

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 include 'libs/dbOperations.php';
 $db= new dbOperations();
-$query=$db->Sql("Select * from projects");
+$query=$db->Sql("SELECT projects.id, projects.name, projects.url, projects.endtime,
+		 		 CONVERT( (SUM( state ) / COUNT( state ) *100 ) , SIGNED) AS ratio
+				 FROM projects
+				 INNER JOIN tasks ON projects.id = tasks.project_id
+				 GROUP BY projects.name
+				 LIMIT 0 , 30");
 include 'libs/parser.php';
 $parser = new parser();
 $sqldatas=$query->fetchAll();
